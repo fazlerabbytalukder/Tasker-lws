14cf86d733f68f6c392f224429f850905810b8fa
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-01-15 20:09:13 +0600

    search

--- src/task/SearchTask.jsx
+++ src/task/SearchTask.jsx
@@ -1,4 +1,12 @@
-export default function SearchTask() {
+import { useState } from "react";
+
+export default function SearchTask({ onSearch }) {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    function handleClick(e) {
+        e.preventDefault();
+        onSearch(searchTerm);
+    }
     return (
         <>
             <form>
@@ -6,8 +14,13 @@ export default function SearchTask() {
                     <div className="relative overflow-hidden rounded-lg text-gray-50 md:min-w-[380px] lg:min-w-[440px]">
                         <input type="search" id="search-dropdown"
                             className="z-20 block w-full bg-gray-800 px-4 py-2 pr-10 focus:outline-none" placeholder="Search Task"
-                            required />
-                        <button type="submit" className="absolute right-2 top-0 h-full rounded-e-lg text-white md:right-4">
+                            required
+                            value={searchTerm}
+                            onChange={() => setSearchTerm(event.target.value)}
+                        />
+                        <button type="submit" className="absolute right-2 top-0 h-full rounded-e-lg text-white md:right-4"
+                            onClick={handleClick}
+                        >
                             <svg className="h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                 viewBox="0 0 20 20">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
